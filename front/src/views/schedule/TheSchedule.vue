9df7156e908312264a2426033ec11f1754ae1606
appleRui <apple31581@gmail.com>
2022-03-19 23:59:13 +0900

    Feature/#27 Change Month or Week

--- front/src/views/schedule/TheSchedule.vue
+++ front/src/views/schedule/TheSchedule.vue
@@ -2,12 +2,15 @@
 .schedule {
   height: 85vh;
 }
+.type-select {
+  max-width: 72px;
+}
 </style>
 
 <template>
   <div class="schedule">
-    <v-sheet tile height="6vh" class="d-flex align-center">
-      <v-btn outlined small class="ma-4" @click="setToday">今日</v-btn>
+    <v-sheet tile height="7vh" class="d-flex align-center">
+      <v-btn outlined class="mx-4" color="gray" @click="setToday">今日</v-btn>
       <v-btn icon @click="$refs.calendar.prev()">
         <v-icon>mdi-chevron-left</v-icon>
       </v-btn>
@@ -15,13 +18,27 @@
         <v-icon>mdi-chevron-right</v-icon>
       </v-btn>
       <v-toolbar-title>{{ title }}</v-toolbar-title>
+      <v-spacer></v-spacer>
+      <v-select
+        class="ma-2 type-select"
+        v-model="type"
+        :items="types"
+        item-text="name"
+        item-value="value"
+        return-object
+        dense
+        outlined
+        hide-details
+      ></v-select>
     </v-sheet>
     <v-calendar
       ref="calendar"
       v-model="value"
       locale="ja-jp"
+      color="primary"
+      :type="type.value"
       :events="events"
-      :event-color="getEventColor"
+      :day-format="(timestamp) => new Date(timestamp.date).getDate()"
       @change="getEvents"
     ></v-calendar>
   </div>
@@ -35,7 +52,9 @@ export default {
   data() {
     return {
       events: [],
-      value: day.format('YYYY-MM-DD')
+      value: day.format('YYYY-MM-DD'),
+      type: {name: '月', value: 'month'},
+      types: [{name: '月', value: 'month'}, {name: '週', value: 'week'}],
     }
   },
   methods: {
@@ -44,15 +63,12 @@ export default {
         {
           name: '会議',
           start: new Date('2022-03-19T01:00:00'),
-          end: new Date('2022-03-20T02:00:00'),
+          end: new Date('2022-03-21T02:00:00'),
           color: 'blue',
           timed: true,
         },
       ];
       this.events = events;
-    },
-    getEventColor(event) {
-      return event.color;
     },
     setToday() {
       this.value = day.format('YYYY-MM-DD')
